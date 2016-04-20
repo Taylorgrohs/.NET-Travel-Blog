c8127e20ace1227b72a5aa424c1756b04b1271d0
michael smith <taylorgrohs@gmail.com>
2016-04-20 16:38:47 -0700

    start experience view crud

---
+++ TravelBlog/src/TravelBlog/Controllers/ExperiencesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using TravelBlog.Models;
+using Microsoft.Data.Entity;
+
+
+
+namespace TravelBlog.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+
+        private TravelBlogContext db = new TravelBlogContext();
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
